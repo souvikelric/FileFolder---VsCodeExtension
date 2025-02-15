3824f5a36d369fb6ed23ef4788c57aa6bfe71c2e
Souvik <souvikelric@gmail.com>
2025-02-16 00:00:22 +0530

    Added user changeable options and events

--- src/extension.ts
+++ src/extension.ts
@@ -1,92 +1,112 @@
 import { error } from "console";
 import * as fs from "fs";
 import * as path from "path";
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
+  //Testing a console log
+  // let folder = vscode.workspace.workspaceFolders?.[0].name
+  // console.log(folder)
+  const statusItem = vscode.window.createStatusBarItem();
 
-	//Testing a console log
-	// let folder = vscode.workspace.workspaceFolders?.[0].name
-	// console.log(folder)
-	const statusItem = vscode.window.createStatusBarItem();
+  const countAndShow = () => {
+    const workFolder = vscode.workspace.workspaceFolders
+      ? vscode.workspace.workspaceFolders[0].uri.fsPath
+      : "";
+    if (!workFolder) {
+      vscode.window.showErrorMessage("No Workspace Folder open in vscode");
+      return;
+    }
+    // vscode.window.showInformationMessage(workFolder);
+    const config = vscode.workspace.getConfiguration("fileandfoldercounter");
+    const color = config.color;
+    const githubIncluded = config.githubIncluded;
+    console.log(githubIncluded);
 
-	const countAndShow = () => {
+    const countFileFolders = (dirPath: string) => {
+      let fileCount = 0;
+      let folderCount = 0;
 
-		const workFolder = vscode.workspace.workspaceFolders ? 
-						   vscode.workspace.workspaceFolders[0].uri.fsPath:"";
-		if(!workFolder){
-			vscode.window.showErrorMessage("Now Workspace Folder open in vscode");
-			return;
-		}
-		// vscode.window.showInformationMessage(workFolder);
+      const items = fs.readdirSync(dirPath);
+      // vscode.window.showInformationMessage(String(items.length));
 
-		const countFileFolders = (dirPath:string) => {
+      items.forEach((item) => {
+        console.log(item);
+        const fullPath = path.resolve(dirPath, item);
+        const stat = fs.statSync(fullPath);
+        if (item === ".git" && githubIncluded === false) {
+          return;
+        } else if (stat.isDirectory()) {
+          folderCount++;
+          const nestedFolders = countFileFolders(fullPath);
+          fileCount += nestedFolders.fileCount;
+          folderCount += nestedFolders.folderCount;
+        } else if (stat.isFile()) {
+          fileCount++;
+        }
+      });
+      return { fileCount, folderCount };
+    };
+    const { fileCount, folderCount } = countFileFolders(workFolder);
+    const outputString = `files : ${fileCount} folders : ${folderCount}`;
 
-			let fileCount = 0;
-			let folderCount = 0;
+    // To show it in the active Terminal
+    //vscode.window.activeTerminal?.sendText(`echo ${outputString}`,true);
+    // vscode.window.setStatusBarMessage(outputString);
 
-			const items = fs.readdirSync(dirPath);
-			// vscode.window.showInformationMessage(String(items.length));
+    const defaultSettings = {
+      color: "#cf59f9",
+      githubIncluded: false,
+    };
 
-			items.forEach(item => {
-				const fullPath = path.resolve(dirPath,item);
-				const stat = fs.statSync(fullPath);
-				if(stat.isDirectory()){
-					folderCount++;
-					const nestedFolders = countFileFolders(fullPath);
-					fileCount += nestedFolders.fileCount;
-					folderCount += nestedFolders.folderCount;
-				}else if(stat.isFile()){
-					fileCount++;
-				}
-			});
-			return {fileCount, folderCount}
+    // console.log(workFolder);
 
-		}
-		const {fileCount, folderCount} = countFileFolders(workFolder);
-		const outputString = `files : ${fileCount}, folders : ${folderCount}`
+    statusItem.backgroundColor = new vscode.ThemeColor("#fff");
+    statusItem.color = color;
+    statusItem.text = `❄️ ${outputString}`;
+    statusItem.show();
 
-		// To show it in the active Terminal
-		//vscode.window.activeTerminal?.sendText(`echo ${outputString}`,true);
-		// vscode.window.setStatusBarMessage(outputString);
+    // const config = vscode.workspace.getConfiguration();
+    // console.log(config);
+  };
 
-		statusItem.backgroundColor = new vscode.ThemeColor("#fff");
-		statusItem.color = "#cf59f9";
-		statusItem.text=`❄️${outputString}`
-		statusItem.show();
+  countAndShow();
 
-		const config = vscode.workspace.getConfiguration();
-		// console.log(config);
+  vscode.workspace.onDidCreateFiles(() => {
+    countAndShow();
+  });
 
-		}
+  vscode.workspace.onDidDeleteFiles(() => {
+    countAndShow();
+  });
 
-	countAndShow()
+  // checking if user changedConfiguration
+  vscode.workspace.onDidChangeConfiguration((e) => {
+    if (e.affectsConfiguration("fileandfoldercounter")) {
+      vscode.window.showInformationMessage(
+        "File&Folder Counter Config changed. Recalculating..."
+      );
+      countAndShow();
+    }
+  });
 
-	vscode.workspace.onDidCreateFiles(()=>{
-		countAndShow();
-	})
+  const disposable = vscode.commands.registerCommand(
+    "fileandfoldercounter.fileFolderCount",
+    () => {
+      // Testing some functions and properties
+      // let currWorkspace = vscode.workspace.name as string
+      // vscode.window.showInformationMessage(currWorkspace);
+      // vscode.window.showErrorMessage("This is a test Error");
 
-	vscode.workspace.onDidDeleteFiles(()=> {
-		countAndShow();
-	})
-	
-	const disposable = vscode.commands.registerCommand('fileandfoldercounter.fileFolderCount', () => {
-		// Testing some functions and properties
-		// let currWorkspace = vscode.workspace.name as string
-		// vscode.window.showInformationMessage(currWorkspace);	
-		// vscode.window.showErrorMessage("This is a test Error");
+      countAndShow();
+    }
+  );
 
-		countAndShow();
-		
-	});
+  // vscode.workspace.onDidChangeWorkspaceFolders(()=>{
+  // 	countAndShow()
+  // })
 
-	// vscode.workspace.onDidChangeWorkspaceFolders(()=>{
-	// 	countAndShow()
-	// })
-
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
-
 
 export function deactivate() {}
